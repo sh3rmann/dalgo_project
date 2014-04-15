351349c4d2bec417bacfd325fe4b84b1b254e6d5
shermann <sven.herrmann@student.jade-hs.de>
2014-04-15 14:18:22 +0200

    Der Start: newscript (Startdatei), Funktion zum Daten aus dem Netz laden erstellt und parse_xml mit eingebunden

---
+++ get_data.m
@@ -0,0 +1,42 @@
+% function [out_param] = get_data()
+% function to do something usefull (fill out)
+% Usage [out_param] = get_data(in_param)
+% Input Parameter:
+%	 in_param: 		 Explain the parameter, default values, and units
+% Output Parameter:
+%	 out_param: 	 Explain the parameter, default values, and units
+%------------------------------------------------------------------------ 
+% Example: Provide example here if applicable (one or two lines) 
+
+% Author: S.Herrmann, J.Klug (c) TGM @ Jade Hochschule applied licence see 
+% EOF 
+% Version History:
+% Ver. 0.01 initial create     14-Apr-2014  Initials (SH,JK)
+
+%------------Your function implementation here--------------------------- 
+url_path = urlwrite ('http://api.met.no/weatherapi/locationforecast/1.8/?lat=53.143889;lon=8.213889','data.xml');
+
+content_ja = xmlread('data.xml');
+
+content_st = parse_xml(content);
+
+
+%--------------------Licence ---------------------------------------------
+% Copyright (c) <2014> S.Herrmann, J.Klug
+% Jade University of Applied Sciences 
+% Permission is hereby granted, free of charge, to any person obtaining 
+% a copy of this software and associated documentation files 
+% (the "Software"), to deal in the Software without restriction, including 
+% without limitation the rights to use, copy, modify, merge, publish, 
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject
+% to the following conditions:
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
