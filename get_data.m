b15b2c646fd8b6c7fd6a6cdfcba3440a260bdd4e
shermann <sven.herrmann@student.jade-hs.de>
2014-04-20 22:24:51 +0200

    Fertigstellen der get_data Funktion: es werden nun insgesamt drei cell-arrays mir den entsprechenden Daten uebergeben: Temperatur, Bewoelkung und Niederschlag.

--- get_data.m
+++ get_data.m
@@ -1,4 +1,4 @@
-% function [temp_min_max, cloud, prec] = get_data()
+% function [temp_min_max, cloud_mean, prec_mean] = get_data()
 % function to do something usefull (fill out)
 % Usage [out_param] = get_data(in_param)
 % Input Parameter:
@@ -39,7 +39,7 @@ counter2 = 1;
 
 % Zählschleife, in der die Temperatur, die Bewölkung und der Niederschlag
 % aus der Struktur content_st herausgesucht werden in den dazugehörigen
-% cell-arrays (temp, cloud, prec) gespeichert werden
+% cell-arrays (temp, cloud, prec) gespeichert werden. 
 for kk = 1:length(content_st.children{1}.children{2}.children)
     
     if strcmp(content_st.children{1}.children{2}.children{kk}.children{1}.children{1}.name,'temperature');
@@ -65,8 +65,8 @@ for kk = 1:length(content_st.children{1}.children{2}.children)
     end
 end   
 
-% Heraussuchen aller Daten (Plural von Datum!)aus dem Temperatur cell-array
-datum_full = regexp(temp,'[0-9]+-[0-9]+-[0-9]+','match');
+% Heraussuchen aller Daten (Plural von Datum!)aus dem Temperatur cell-array 
+datum_full1 = regexp(temp,'[0-9]+-[0-9]+-[0-9]+','match');
 
 % Erstellen eines neuen cell-arrays, in den die herausgesuchten Daten 
 % geschrieben werden können. 
@@ -75,19 +75,25 @@ datum = cell(1);
 % Umschreiben der Daten. Dies ist notwendig, da der Befehl "regexp" alle 
 % gesuchten Daten (chars)in einen zusätzlichen 1x1 cell-array schreibt. 
 % Dieser wird hier entfernt
-for kk = 1:length(datum_full)
-    datum(kk) = datum_full{kk};
+for kk = 1:length(datum_full1)
+    datum(kk) = datum_full1{kk};
 end
 
 % Alle doppelten Daten werden hier entfernt
 datum = unique(datum);
 
-% Schleife, die alle einstelligen Temperaturwerte mit einer Null ergänzt.
-% Dies ist notwendig, um die im cell-array gespeicherten Daten später in 
-% eine Matrix wandeln zu können (cell2mat)
+% Schleife, die alle einstelligen Temperatur-/Bewölkungswerte mit einer 
+% Null ergänzt. Dies ist notwendig, um die im cell-array gespeicherten 
+% Daten später in eine Matrix wandeln zu können (cell2mat)
 for kk = 1:length(temp(:,3))
-    if length(temp{kk,3})<4
-       temp{kk,3} = ['0',temp{kk,3}];
+    if length(temp{kk,3}) == 3
+       temp{kk,3} = [temp{kk,3},'0'];
+    end
+    if length(cloud{kk,3}) == 4
+       cloud{kk,3} = [cloud{kk,3},'0']; 
+    end
+    if length(cloud{kk,3}) == 3
+       cloud{kk,3} = [cloud{kk,3},'00']; 
     end
 end
 
@@ -95,25 +101,65 @@ end
 % Maximaltemperatur und das Datum der nächsten 10 Tage gespeichert werden 
 temp_min_max = cell(length(datum),3);
 
-% Schleife, in der die Temperaturdaten von jedem Datum (datum) 
+% Erstellen eines neuen cell-arrays, in den der mittlere Bewölkungswert und
+% das Datum der nächsten 10 Tage gespeichert wird
+cloud_mean = cell(length(datum),2);
+
+% Schleife, in der die Temperatur-/ Bewölkungsdaten von jedem Datum  
 % herausgesucht werden. Anschließend werden diese in eine Matrix und von 
-% dort aus in Zahlen gewandelt. Von jedem Tag wird dann die Minimal- und
-% die Maximaltemperatur bestimmt und zusammen mit dem entsprechendem Datum
-% gespeichert.
+% dort aus in Zahlen gewandelt. Von jedem Tag wird dann die Minimal-/
+% Maximaltemperatur und der mittlere Bewölkungswert bestimmt und zusammen
+% mit dem entsprechendem Datum gespeichert.
 for kk = 1:length(datum)
     
     row1 = ~cellfun(@isempty, regexp(temp(:,1), datum(kk)));
     row2 = find(row1 == 1);
+    
     temp_all1 = cell2mat(temp(row2,3)); 
     temp_all2 = str2num(temp_all1);
+    cloud_all1 = cell2mat(cloud(row2,3));
+    cloud_all2 = str2num(cloud_all1);
+    
     temp_min = min(temp_all2);
     temp_max = max(temp_all2);
+    cloud_mean_day = mean(cloud_all2); 
+    
     temp_min_max{kk,1} = datum{kk}; 
     temp_min_max{kk,2} = temp_min;
     temp_min_max{kk,3} = temp_max;
+    cloud_mean{kk,1} = datum{kk};
+    cloud_mean{kk,2} = cloud_mean_day;
     
 end
 
+% Erstellen eines neuen cell-arrays, in den der mittlere Niederschlagswert
+% und das Datum der nächsten 10 Tage gespeichert wird 
+prec_mean = cell(length(datum),2);
+
+% Schleife, in der alle Niederschlagswerte pro Tag herausgesucht werden.
+% Anschließend werden diese in eine Matrix und von dort aus in Zahlen 
+% umgewandelt. Zum Schluss wird pro Tag der Mittelwert gebildet und
+% zusammen mit dem entsprechendem Datum in dem cell-array gespeichert.
+% Anmerkung: da die Niederschlagswerte nicht stündlich -wie Temperatur und
+% Bewökung- vorliegen, sondern in unterschiedlichen Intervallen, ist die
+% Datenmenge entsprechend größer. Daher werden für die Zuordnung zu den
+% jeweiligen Tagen andere Zeilenangaben benötigt. Um die Übersichtlichkeit
+% zu wahren, findet dieser Arbeitsschritt in einer gesonderten 
+% Schleife statt
+for kk = 1:length(datum)
+    
+    row1 = ~cellfun(@isempty, regexp(prec(:,1), datum(kk)));
+    row2 = find(row1 == 1);
+    
+    prec_all1 = cell2mat(prec(row2,3));
+    prec_all2 = str2num(prec_all1);
+    
+    prec_mean_day = mean(prec_all2);
+    
+    prec_mean{kk,1} = datum{kk};
+    prec_mean{kk,2} = prec_mean_day;
+
+end
 %--------------------Licence ---------------------------------------------
 % Copyright (c) <2014> S.Herrmann, J.Klug
 % Jade University of Applied Sciences 
