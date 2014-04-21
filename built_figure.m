417d31651234b3db1a7732d5a54e0b1e0e2010ad
shermann <sven.herrmann@student.jade-hs.de>
2014-04-21 17:35:35 +0200

    Bei der Funktion built_figure den ersten Tag fertig gestellt: Es werden jetzt Temperarur, Bewölkung und Niederschlag grafisch dargestellt. Zudem die Niederschlagsgrafik hochgeladen.

--- built_figure.m
+++ built_figure.m
@@ -1,5 +1,5 @@
-% function [out_param] = built_figure(temp, cloud, prec)
-% function to do something usefull (fill out)
+% function [] = built_figure(temp, cloud, prec)
+% function to built a figure and show weather forecast grafically
 % Usage [out_param] = built_figure(in_param)
 % Input Parameter:
 %	 in_param: 		 Explain the parameter, default values, and units
@@ -17,6 +17,7 @@
 
 % [temp, cloud, prec] = get_data();
 
+%%%%%%%%%%%%%%%%%%%%%%%%%% Erstellen der figure %%%%%%%%%%%%%%%%%%%%%%%%%%
 figure_handle = figure();
 
 set(figure_handle,'Name','Weatherdata from Oldenburg','NumberTitle','off');
@@ -26,23 +27,27 @@ set(figure_handle,'ToolBar','none');
 set(figure_handle,'MenuBar','none');
 set(figure_handle,'Color',[1 1 1 ]);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Tag 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Datum
 h1_text1 = uicontrol('style','text');
 set(h1_text1,'units','normalized');
 set(h1_text1,'position',[0.04 0.87 0.15 0.075]);
 set(h1_text1,'BackgroundColor',[1 1 1]);
-set(h1_text1,'FontName','Comic Sans MS')
+set(h1_text1,'FontName','Comic Sans MS');
 set(h1_text1,'FontSize',20);
 set(h1_text1,'string',temp{1,1});
 
+% Bild Thermometer
 thermo = imread('thermometer.jpg');
-axes('Position', [0.04 0.6 0.03 0.15]); 
+axes('Position', [0.04 0.65 0.03 0.15]); 
 image(thermo);
 axis image;
 axis off;
 
+% Maximaltemperatur in Grad
 h1_text2 = uicontrol('style','text');
 set(h1_text2,'units','normalized');
-set(h1_text2,'position',[0.09 0.67 0.1 0.07]);
+set(h1_text2,'position',[0.09 0.72 0.1 0.07]);
 set(h1_text2,'BackgroundColor',[1 1 1]);
 set(h1_text2,'FontName','Arial')
 set(h1_text2,'FontSize',22);
@@ -51,17 +56,19 @@ h1_value1 = num2str(temp{1,3});
 h1_value1 = [h1_value1,'°'];
 set(h1_text2,'string',h1_value1);
 
+% Minimaltemperarur in Grad
 h1_text3 = uicontrol('style','text');
 set(h1_text3,'units','normalized');
-set(h1_text3,'position',[0.09 0.6 0.1 0.07]);
+set(h1_text3,'position',[0.09 0.65 0.1 0.07]);
 set(h1_text3,'BackgroundColor',[1 1 1]);
-set(h1_text3,'FontName','Arial')
+set(h1_text3,'FontName','Arial');
 set(h1_text3,'FontSize',22);
 set(h1_text3,'ForegroundColor',[0 1 0]);
 h1_value2 = num2str(temp{1,2});
 h1_value2 = [h1_value2,'°'];
 set(h1_text3,'string',h1_value2);
 
+% Wolkenbild, abhängig von der Stärke der Bewölkung
 h1_value3 = round(cloud{1,2});
 if h1_value3 >= 0 && h1_value3 < 25
    wolke = imread('wolke1.jpg');
@@ -79,22 +86,43 @@ if h1_value3 >= 75 && h1_value3 <= 100
    wolke = imread('wolke4.jpg');
    font_color = [0 0 1];
 end
-
-axes('Position', [0.04 0.38 0.1 0.15]); 
+axes('Position', [0.05 0.43 0.1 0.15]); 
 image(wolke);
 axis image;
 axis off;
 
+% Bewölkungsgrad in Prozent
 h1_text4 = uicontrol('style','text');
 set(h1_text4,'units','normalized');
-set(h1_text4,'position',[0.04 0.3 0.1 0.065]);
+set(h1_text4,'position',[0.05 0.35 0.1 0.065]);
 set(h1_text4,'BackgroundColor',[1 1 1]);
-set(h1_text4,'FontName','Arial')
+set(h1_text4,'FontName','Arial');
 set(h1_text4,'FontSize',22);
 set(h1_text4,'ForegroundColor',font_color);
 h1_value3 = num2str(h1_value3);
 h1_value3 = [h1_value3,'%'];
 set(h1_text4,'string',h1_value3);
+
+% Niederschlagsbild
+niederschlag = imread('niederschlag.jpg');
+axes('Position', [0.05 0.13 0.1 0.15]); 
+image(niederschlag);
+axis image;
+axis off;
+
+% Niederschlagswert in mm
+h1_value4 = (round(prec{1,2}*100))/100;
+h1_text5 = uicontrol('style','text');
+set(h1_text5,'units','normalized');
+set(h1_text5,'position',[0.05 0.03 0.1 0.075]);
+set(h1_text5,'BackgroundColor',[1 1 1]);
+set(h1_text5,'FontName','Arial');
+set(h1_text5,'FontSize',19);
+set(h1_text5,'ForegroundColor',[0.1 0 1]);
+h1_value4 = num2str(h1_value4);
+h1_value4 = [h1_value4,'mm'];
+set(h1_text5,'string',h1_value4);
+
 
 %--------------------Licence ---------------------------------------------
 % Copyright (c) <2014> S.Herrmann, J.Klug
